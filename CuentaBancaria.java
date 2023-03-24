6c22a204a1e719b837bfba825af00f27e3f8b92b
Miguel Timur Lozano <timi1484@gmail.com>
2023-03-24 16:59:18 +0100

    Modificacion main creacion nuevo objeto

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -3,6 +3,7 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("0234521","Director Banco",100,0);
+		CuentaBancaria cuenta = new CuentaBancaria("4321521","Timur",200,10);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
