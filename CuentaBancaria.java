d7b74080bd11086404eb6a7764b3f5d19ee9c73f
Miguel Timur Lozano <timi1484@gmail.com>
2023-03-24 17:09:42 +0100

    Cambio en la 2 rama

--- CuentaBancaria.java
+++ CuentaBancaria.java
@@ -3,6 +3,7 @@ class TestCuentaBancaria
 	public static void main(String args[])
 	{
 		CuentaBancaria cuenta = new CuentaBancaria("0234521","Director Banco",100,0);
+		CuentaBancaria cuenta = new CuentaBancaria("0123456789","Miguel Timur Lozano",1000,5);
 		System.out.println("Saldo = " + cuenta.getSaldo());
 		cuenta.reintegro(50);
 		cuenta.ingreso(100);
